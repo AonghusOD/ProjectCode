d2376603d7132d6a9811ab19a2d517b3c257aac2
Aonghus <g00293306@gmit.ie>
2022-03-31 15:20:24 +0100

    Getting data from DB with docs

--- Site/next/nextjs-blog/components/air/AirQuality.js
+++ Site/next/nextjs-blog/components/air/AirQuality.js
@@ -1,39 +1,38 @@
 import { React, useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
-import { useHistory } from "react-router-dom";
 
-function AirQuality() {
+const AirQuality = () =>{
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(false)
-
+  
   useEffect(() => {
     setLoading(true)
     fetch('api/getAir')
       .then((res) => res.json())
       .then((data) => {
+        const air = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+            
+          };
+          console.log(meetup)
+          air.push(meetup);
+        }
+        console.log(data)
         setData(data)
+        //console.log(air)
+        
         setLoading(false)
       })
   }, [])
 
+  
+
   if (isLoading) return <p>Loading...</p>
   if (!data) return <p>No profile data</p>
-
-  //////////////////////////////////////////////////
-  // const [dataObj, setDataObj] = useState();
-
-  // useEffect(() => {
-  //   fetch("api/getAir")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setDataObj((oldDataObj) => {
-  //         let prevDataObj = JSON.parse(JSON.stringify(oldDataObj));
-  //         prevDataObj.noMessages = data.noMessages;
-  //         return prevDataObj;
-  //       });
-  //     });
-  // }, []);
-  ///////////////////////////////////////////////////////
 
   const data1 = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
@@ -57,21 +56,17 @@ function AirQuality() {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [53, data.airReading, 80, 81, 56, 55, 40],
+        data: [53, data.airReading, 80, 56, 56, 55, 40],
       },
     ],
   };
+  
 
   return (
     <section>
       <div>
         <h2>AirQuality Data</h2>
         <h1>{data.airReading}</h1>
-        {/* <ul>
-          {air.map((meetup) => {
-            <li>{meetup.airReading}</li>;
-          })}
-        </ul> */}
         <div
           style={{
             height: "40vh",
