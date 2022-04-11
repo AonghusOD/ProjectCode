71194ecaa45a54a87690e70a4b9fb1bdf9ce6991
Aonghus <g00293306@gmit.ie>
2022-04-11 15:43:11 +0100

    IOT Sending Messages

--- Site/node/node-blog/routes/index.js
+++ Site/node/node-blog/routes/index.js
@@ -2,6 +2,18 @@ let express = require("express");
 let router = express.Router();
 let mongoose = require("mongoose");
 mongoose.connect("localhost:27017/blog");
+const awsIot = require('aws-iot-device-sdk');
+
+const device = awsIot.device({
+    clientId: 'AonghusMQTT', //AonghusMQTTClient  Your own device name . . . whatever you like
+    host: 'a8p9udme32jcv-ats.iot.eu-west-1.amazonaws.com', // your IOT setup
+    //port: 8883,
+    keyPath: './certs/cb200e58a2a2c0207863b7399b96b9f94c483151025c093179680a23bd02184c-private.pem.key', // put these files in same directory as this file
+    certPath: './certs/cb200e58a2a2c0207863b7399b96b9f94c483151025c093179680a23bd02184c-certificate.pem.crt',
+    caPath: './certs/AmazonRootCA1.pem',
+});
+
+
 let Schema = mongoose.Schema;
 
 const airSchema = new Schema(
@@ -59,5 +71,33 @@ router.post("/getAir", function (req, res, next) {
     res.status(200).json(docs)
   });
 });
+
+function sendData(topicName, data) {
+  console.log("STEP - Sending data to AWS  IoT Core: " + JSON.stringify(data))
+  console.log("---------------------------------------------------------------------------------")
+  return device.publish(topicName, JSON.stringify(data)) //  "signInEtc" in the topic
+}
+
+// We connect our client to AWS  IoT core. 
+device
+  .on("connect", function () {
+      console.log("STEP - Connecting to AWS  IoT Core");
+      console.log("---------------------------------------------------------------------------------")
+      device.subscribe("esp32/pub");
+    //  sendData("signInEtcTopic", {connectStatus: "node.js is connected"})
+  });
+
+// Set handler for the device, it will get the messages from subscribers topics.
+device
+  .on("message", function (topic, payload) {
+      console.log('message', topic, payload.toString());
+  });
+
+device
+  .on("error", function (topic, payload) {
+      console.log('Error:', topic, payload.toString());
+  });
+
+
 
 module.exports = router;
