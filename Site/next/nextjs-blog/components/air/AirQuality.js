e71a86a1336a8c59fc06a5f985330f808382782a
Aonghus <g00293306@gmit.ie>
2022-04-11 15:11:03 +0100

    Before adding code for IOT Core

--- Site/next/nextjs-blog/components/air/AirQuality.js
+++ Site/next/nextjs-blog/components/air/AirQuality.js
@@ -56,7 +56,7 @@ const AirQuality = () =>{
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [53, data.airReading, 80, 56, 56, 55, 40],
+        data: [data],
       },
     ],
   };
